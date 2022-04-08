21c8db9a2beaa24ad64c42b4ad26af9e01ffaf14
Buhbah6 <thegoldenrailway@gmail.com>
2022-04-08 03:52:44 -0400

    Added Firebase connectivity

--- program/LGMM/app/src/main/java/com/appdev/lgmm/MainActivity.java
+++ program/LGMM/app/src/main/java/com/appdev/lgmm/MainActivity.java
@@ -1,14 +1,62 @@
 package com.appdev.lgmm;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    List<AuthUI.IdpConfig> providers;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.GenericOAuthProviderBuilder(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.GitHubBuilder().build());
+
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        signInLauncher.launch(signInIntent);
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            user = FirebaseAuth.getInstance().getCurrentUser();
+        }
+        else {
+            Toast.makeText(this, "SignUp/SignIn Failed. Please try again.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
