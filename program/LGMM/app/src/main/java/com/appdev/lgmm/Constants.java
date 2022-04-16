42cae2f2eb55c6dfa7f5aae4432df5aca755d384
Giuliana Bouzon <57763699+gbouzon@users.noreply.github.com>
2022-04-15 22:52:23 -0400

    added constants file

---
+++ program/LGMM/app/src/main/java/com/appdev/lgmm/Constants.java
@@ -0,0 +1,8 @@
+package com.appdev.lgmm;
+
+public interface Constants {
+    String APP_ID = "xxx-xxx-xxx";
+    String AUTH_KEY = "xxx-xxx-xxx";
+    String REGION = "xxx-xxx-xxx";
+    String API_KEY = "xxx-xxx-xxx";
+}
